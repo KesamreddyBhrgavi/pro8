57d14917104f2bbb02980efd83f938686fb93349
kesamreddy Bhargavi <kesamreddybhargavi2@gmail.com>
2023-06-06 11:07:51 +0530

    first commit

---
+++ csk/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def goutham(request):
+    return render(request,'goutham.html')
+
+def srujana(request):
+    return render(request,'srujana.html')
